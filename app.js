c5281c79541bc317363d05e667c79ae98c7e3b30
felprangel <feliperangel897@gmail.com>
2024-08-29 21:04:49 -0300

    utiliza arquivo de rotas

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("usernames will be logged here - wip");
-});
+const userRouter = require("./routes/user");
 
-app.get("/new", (req, res) => {
-  res.sendFile("./newUser.html");
-});
-
-app.post("/new", (req, res) => {
-  console.log("username to be saved: ", req.body.username);
-});
+app.use("/", userRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
