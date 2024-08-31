b798e81eabd4a1b1ba2249156fb4c28cbd3820cd
felprangel <feliperangel897@gmail.com>
2024-08-31 16:53:22 -0300

    adiciona pesquisa por parametro de url

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,13 @@ const path = require("path");
 const db = require("../db/queries");
 
 exports.getAllUsers = async (req, res) => {
+  if (req.query.search) {
+    const usernames = await db.getUsernamesBySearch(req.query.search);
+    console.log("usernames: ", usernames);
+    return res.send(
+      "Usernames: " + usernames.map((user) => user.username).join(", ")
+    );
+  }
   const usernames = await db.getAllUsernames();
   console.log("usernames: ", usernames);
   res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
