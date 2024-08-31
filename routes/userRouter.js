5f2d4c37ec3ac7c6202e4dfa766ae64c7a53f3c8
felprangel <feliperangel897@gmail.com>
2024-08-31 17:05:05 -0300

    adiciona rota para deletar todos os usuários

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,5 +5,6 @@ const userRouter = Router();
 userRouter.get("/", userController.getAllUsers);
 userRouter.get("/new", userController.getNewUser);
 userRouter.post("/new", userController.createUser);
+userRouter.delete("/", userController.deleteUser);
 
 module.exports = userRouter;
